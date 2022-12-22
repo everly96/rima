16ee0921cbec8c6cee0ee05007a54e8fc254f641
rima <bu960513@naver.com>
2022-12-22 17:31:52 +0900

    3

--- html/js/works.js
+++ html/js/works.js
@@ -1,33 +1,15 @@
-;(function($){
-    $(function(){
-        var swiper = new Swiper('.swiper-container', {
-            scrollbar: {
-              el: '.swiper-scrollbar',
-              hide: true,
-              },
+(function ($) {
+  $(function () {
+    var swiper = new Swiper(".swiper-container", {
+      scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+      },
 
-            navigation: {
-              nextEl: '.swiper-button-next',
-              prevEl: '.swiper-button-prev',
-            },
-          });
-
-        $(window).on('load resize',function(){
-
-          $('.page_f').each(function(){
-            
-            var w=$(this).width();
-            var h=$(this).height();
-            var l=$(this).find('iframe').attr('width');
-            var s=parseInt(l);
-            var u=w/s;
-            
-            $(this).find('iframe').css('transform','scale('+u+','+u+')');
-            $(this).find('iframe').css('height',h/u);
-
-          });
-
-        });
-
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
     });
+  });
 })(jQuery);
